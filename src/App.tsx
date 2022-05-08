aea3fa0fc5da563a3c3447a1cc0cec75dc2d495b
alamgirjuab <alamgirjuab@gmail.com>
2022-05-09 03:03:56 +0600

    banner created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Banner from './components/Home/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Welcome to AAMAR BANK</h2>
-      </header>
+      <Banner />
     </div>
   );
 }
